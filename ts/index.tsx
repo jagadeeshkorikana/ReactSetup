80d008291d719249abd8ae33a5434a6bffbf6ee2
Jagadeesh Korikana <v-jakori@CRMIDCDEVDOM.extest.microsoft.com>
2017-06-20 19:33:06 +0530

    Stateless Component Example updated

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,17 +1,57 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
-
+interface IProps {};
+interface IState {};
 let Jagadeesh=()=>{
     return <div>
         <Hari/>
-        <h1>Jagadeeshkorikana</h1>
+        <h1>Footerone</h1>
     </div>
 }
 
 let Hari=()=>{
     return <div>
-        <h2>Hari Krishna</h2>
+        <h2>Footertwo</h2>
     </div>
 }
 
-ReactDom.render(<Jagadeesh/>,document.getElementById("DemoApp"));
+class DemoApp extends React.Component<IProps, IState> {
+    render()
+    {
+        return (
+        <div>
+            <Header/>
+            <Content/>
+            <Jagadeesh/>
+        </div>
+        );
+    }
+}
+
+class Header extends React.Component<IProps, IState>{
+    render()
+    {
+        return (
+            <div>
+                <h1>Heading</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component<IProps, IState>{
+    render()
+    {
+        return(
+            <div>
+                <h2>Content Area</h2>
+            </div>
+        );
+    }
+}
+//ReactDom.render(<Jagadeesh/>,document.getElementById("DemoApp"));
+//export default DemoApp;
+
+ReactDom.render(
+	<DemoApp />, document.getElementById("DemoApp")
+)
